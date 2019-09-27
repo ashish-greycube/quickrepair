0d5f6870fa75a4733948f074484385281d15fddd
ashish-greycube <mr.ashish.shah@gmail.com>
2019-09-27 11:47:24 +0530

    search box on items page

---
+++ quickrepair/templates/includes/item_search.js
@@ -0,0 +1,14 @@
+frappe.ready(function () {
+$('input[type=search]').on('keydown', (e) => {
+	if (e.keyCode === 13) {
+	  // Enter
+	  const value = e.target.value;
+	  if (value) {
+		console.log(window.location.protocol+'//'+ window.location.host+'/all-products?search=' + e.target.value)
+		window.location.href=window.location.protocol+'//'+ window.location.host+'/all-products?search=' + e.target.value;
+	  } else {
+		// window.location.search = '';
+	  }
+	}
+  });
+});    
